4c82e16a4d31b36d6d12061f5b49605db06640bf
slcassidy <samantha.l.cassidy@gmail.com>
2019-02-20 21:29:13 -0500

    Upload 20190220

--- unsolved/app.js
+++ unsolved/app.js
@@ -1,10 +1,36 @@
 // Declare variables
 
+// hide the entire screen of view in left panel
+(function(){
+  $(`.content`).hide();
+  $(`#name`).hide();
+  $(`#officenum`).hide();
+  $(`#phoneNum`).hide(); 
+  $(`#search`).hide();
+  $('#addRecord').hide();
+  $('#deleteRecord').hide();  
+  $('#verifyRecord').hide();
+  $('#updateRecord').hide();
+})();
 
 
-// Allowing the list to be visiable on the page **aka rendered on the page**
-const render = function() {
-    $('.content').empty();
+// const hideAll = function(){
+//   $(`.content`).hide();
+//   $(`#name`).hide();
+//   $(`#officenum`).hide();
+//   $(`#phoneNum`).hide(); 
+//   $(`#search`).hide();
+//   $('#addRecord').hide();
+//   $('#deleteRecord').hide();  
+//   $('#verifyRecord').hide();
+// };
+
+// hideAll();
+
+const showEmployeeList = function(){
+  $(`.content`).show();
+  const render = function() {
+    // $('.content').empty();
   
     // Adding the card to have a box around the names
     for( let i = 0; i < employeeList.length; i++ ) {
@@ -19,24 +45,103 @@ const render = function() {
   }
   
   render();
+}
 
-// Clean version testing to make sure the variables are brought over
-//   const render = function() {
-  
-//     for( let i = 0; i < employeeList.length; i++ ) {
-//       $('.content').append(`<p>${employeeList[i].name}</p>`);
-//       $('.content').append(`<p>${employeeList[i].officeNum}</p>`);
-//       $('.content').append(`<p>${employeeList[i].phoneNum}</p>`);  
+// Show main variables to search on
+const showVar = function(){
+  $(`#name`).show();
+  $(`#officenum`).show();
+  $(`#phoneNum`).show(); 
+};
 
-//     }
-//   }
-  
-//   render();
+// Show header of selections to add records
+const showAdd = function(){
+  // $(`#name`).show();
+  // $(`#officenum`).show();
+  // $(`#phoneNum`).show(); 
+  $('#addRecord').show();
+  $('#deleteRecord').hide();
+  $('#updateRecord').hide();
+  $('#search').hide();
+};
+
+// Show header of selections to delete records
+const showDelete = function(){
+  // $(`#name`).show();
+  // $(`#officenum`).show();
+  // $(`#phoneNum`).show(); 
+  $('#deleteRecord').show();
+  $('#addRecord').hide();
+  $('#updateRecord').hide();
+  $('#search').hide();
+};
+
+// Show header of selections to verify name of record
+const showVerify = function(){
+  // $(`#name`).show();
+  // $(`#officenum`).hide();
+  // $(`#phoneNum`).hide(); 
+  $('#addRecord').hide();
+  $('#updateRecord').hide();
+  $('#deleteRecord').hide();
+  $('#search').show();
+};
+
+// Update the header to show
+const showUpdate = function(){
+  // $(`#name`).show();
+  // $(`#officenum`).hide();
+  // $(`#phoneNum`).hide(); 
+  $('#addRecord').hide();
+  $('#updateRecord').show();
+  $('#deleteRecord').hide();
+  $('#search').hide();
+};
 
 
-(function(){
-    $(`#name`).hide();
-})();
+// Show the View of the list only
+$('#view').on('click',function(){
+  $(`#search`).hide();
+  $('#addRecord').hide();
+  $('#deleteRecord').hide();  
+  $('#verifyRecord').hide();
+  $('#updateRecord').hide();
+  showEmployeeList();
+});
+
+// Show the view of add once link clicked
+$('#add').on('click', function(){
+  showVar()
+  showAdd()
+  showEmployeeList()
+});
+
+// Show the view of delete once link clicked
+$('#delete').on('click', function(){
+  showVar()
+  showDelete()
+  showEmployeeList()
+});
+
+
+// Show view of item to verify
+$('#verify').on('click',function(){
+  showVar()
+  showVerify()
+  showEmployeeList()
+});
+
+// show view of the update
+$('#update').on('click',function(){
+  showVar()
+  showUpdate()
+  showEmployeeList()
+});
+
+
+
+
+
 
 
 
